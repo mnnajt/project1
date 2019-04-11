7c69ca90640dacacdd29856b489c3fddb8faeaa6
Michał Kaliniak <mkaliniak@smi.pl>
2019-04-11 13:05:53 +0200

    empty project and .gitignore

---
+++ project1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  project1
+//
+//  Created by Michał Kaliniak on 11/04/2019.
+//  Copyright © 2019 MNNAJT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
